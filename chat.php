5e9d8d3919fa46b0529003903c52e7d2a2da22dc
Tống Đức Nam <637849@sv.vnua.edu.vn>
2021-11-30 18:11:31 +0700

    Chat App is updatinggit add .!

---
+++ chat.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RealTime Chat App | CodingNepal</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="262305375_461667835319762_4885023810843628941_n.jpg" alt="">
+                <div class="details">
+                    <span>Cristiano Ronaldo</span>
+                    <p>Active now</p>
+                </div>
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+           
+                <div class="chat incoming">
+                    <img src="262305375_461667835319762_4885023810843628941_n.jpg" alt="">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+           
+                <div class="chat incoming">
+                    <img src="262305375_461667835319762_4885023810843628941_n.jpg" alt="">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+           
+                <div class="chat incoming">
+                    <img src="262305375_461667835319762_4885023810843628941_n.jpg" alt="">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+           
+                <div class="chat incoming">
+                    <img src="262305375_461667835319762_4885023810843628941_n.jpg" alt="">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+           
+                <div class="chat incoming">
+                    <img src="262305375_461667835319762_4885023810843628941_n.jpg" alt="">
+                    <div class="details">
+                        <p>Chat Application using PHP with MySQL & JavaScript</p>
+                    </div>
+                </div>
+            </div>
+            <form action="#" class="typing-area">
+                <input type="text" placeholder="Type a message here...">
+                <button><i class="fab fa-telegram-plane"></i></button>
+            </form>
+        </section>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RealTime Chat App | CodingNepal</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <header>
+                RealTime Chat App
+            </header>
+            <form action="#" enctype="multipart/form-data">
+                <div class="error-txt"></div>
+                <div class="name-details">
+                    <div class="field input">
+                        <label>First Name</label>
+                        <input type="text" name ="fname" placeholder="First Name" required>
+                    </div>
+                    <div class="field input">
+                        <label>Last Name</label>
+                        <input type="text" name ="lname" placeholder="Last Name" required>
+                    </div>
+                </div>
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" name ="email" placeholder="Enter your Email" required>
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" name ="password" placeholder="Enter your Password" required>
+                    <i class="fas fa-eye"></i>
+                </div>
+                <div class="field image">
+                    <label>Select Image</label>
+                    <input type="file" name ="image" required>
+                </div>
+                <div class="field button">
+                    <input type="submit" value="Continue to Chat"/>
+                </div>
+            </form>
+            <div class="link">Already signed up? <a href="#">Login now</a></div>
+        </section>
+    </div>
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/signup.js"></script>
+</body>
+</html>
